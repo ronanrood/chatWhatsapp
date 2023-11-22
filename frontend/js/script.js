89f051073254d69a0106e41348649b0471dcc38f
ronanrood <ronanornelas321@gmail.com>
2023-11-22 19:20:27 -0300

    add elementChat

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -8,6 +8,7 @@ const loginInput = login.querySelector(".login__input")
 const chat = document.querySelector(".chat")
 const chatForm = chat.querySelector(".chat__form")
 const chatInput = chat.querySelector(".chat__input")
+const chatMessages = chat.querySelector(".chat__messages")
 
 const colors =[
     "cadetblue",
@@ -22,12 +23,27 @@ const user={id:"", name:"", color:""}
 
 let websocket
 
+const createMessageSelfElement = (content) => {
+    const div = document.createElement("div")
+
+    div.classList.add("message--self")
+    div.innerHTML = content
+
+    return div
+}
+
 const getRandomColor = () => {
     const randomIndex = Math.floor(Math.random() * colors.length)
     return colors[randomIndex]
 }
 
+const processMessage = ({ data }) => {
+    const { userId, userName, userColor, content } = JSON.parse(data)
 
+    const element = createMessageSelfElement(content)
+
+    chatMessages.appendChild(element)
+}
 
 const handleLogin = (event) => {
     event.preventDefault()
@@ -41,7 +57,23 @@ const handleLogin = (event) => {
 
     websocket = new WebSocket("ws://localhost:8080")
     websocket.onmessage = processMessage
-    console.log(user)
 }
 
+const sendMessage = (event) => {
+    event.preventDefault()
+
+    const message = {
+        userId: user.id,
+        userName: user.name,
+        userColor: user.color,
+        content: chatInput.value
+    }
+
+    websocket.send(JSON.stringify(message))
+
+    chatInput.value = ""
+}
+
+
 loginForm.addEventListener("submit", handleLogin)
+chatForm.addEventListener("submit", sendMessage)
