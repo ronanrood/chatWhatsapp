c308a0cd87176149d2e8c765aacdb87ff583f203
ronanrood <ronanornelas321@gmail.com>
2023-11-22 12:16:06 -0300

    js Login

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,11 @@
-const {WebSocketServer} = require("ws")
-const dotenv = require(dotenv)
+const { WebSocketServer } = require("ws")
+const dotenv = require("dotenv")
 
-dotenv.config()
+dotenv.config() 
 
 const wss = new WebSocketServer({port:process.env.PORT || 8080})
 
 wss.on("connection", (ws)=> {
-
     ws.on("error", console.error)
     ws.on("message", (data)=>{
         wss.clients.forEach((clients) => clients.send(data.toString()))
--- frontend/js/script.js
+++ frontend/js/script.js
@@ -3,6 +3,12 @@ const login = document.querySelector(".login")
 const loginForm = login.querySelector(".login__form")
 const loginInput = login.querySelector(".login__input")
 
+
+// chat elements
+const chat = document.querySelector(".chat")
+const chatForm = chat.querySelector(".chat__form")
+const chatInput = chat.querySelector(".chat__input")
+
 const colors =[
     "cadetblue",
     "darkgoldenrod",
@@ -12,18 +18,30 @@ const colors =[
     "gold",
 ]
 
-const getRandomColor = (colors)=>{
+const user={id:"", name:"", color:""}
+
+let websocket
+
+const getRandomColor = () => {
     const randomIndex = Math.floor(Math.random() * colors.length)
     return colors[randomIndex]
 }
-const user={id:"", name:"", color:""}
 
-const handleSubmit = (event) =>{
+
+
+const handleLogin = (event) => {
     event.preventDefault()
 
     user.id = crypto.randomUUID()
     user.name = loginInput.value
     user.color = getRandomColor()
+
+    login.style.display = "none"
+    chat.style.display = "flex"
+
+    websocket = new WebSocket("ws://localhost:8080")
+    websocket.onmessage = processMessage
+    console.log(user)
 }
 
-loginForm.addEventListener("subimit", handleSubmit)
+loginForm.addEventListener("submit", handleLogin)
